2dec780ec100c29afd86bf50b6cfec8d697d1c62
97jsantos <97jsantos@gmail.com>
2022-06-30 19:53:27 -0300

    att 30/06
    Projeto Costs adicionado aos Projetos

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,6 @@
 import { ProjectsCard } from "./ProjectsCard";
 import salestore from "../img/salestore.png"
+import costs from "../img/costs.png"
 
 export function Projects() {
     return (
@@ -7,8 +8,14 @@ export function Projects() {
             <div className="max-w-7xl w-full">
                 <p className="text-center text-5xl sm:mt-24 mt-14 sm:mb-20 mb-10">Projetos</p>
                 <div className="flex items-center flex-wrap sm:justify-between justify-center">
+                <ProjectsCard
+                    githubLink="https://github.com/97jsantos/costs-project"
+                    projectLink="https://costs-97jsantos.netlify.app/"
+                    bg={costs}
+                    title='Costs'
+                    text="O Costs foi produzido durante o curso de React do Hora de Codar, o intuito foi pôr em prática os ensinamentos de React básico e consumo de API." />
                     <ProjectsCard
-                    githubLink="https://github.com/97jsantos/salestore"
+                    githubLink="https://github.com/97jsantos/salestore-project"
                     projectLink="https://salestore-97jsantos.netlify.app/"
                     bg={salestore}
                     title='SaleStore'
